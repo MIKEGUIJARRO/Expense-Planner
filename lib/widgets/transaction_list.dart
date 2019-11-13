6518b6a657d2779b490c871124d53e8af68fa057
Mike Guijarro <mguijarro39@gmail.com>
2019-11-13 14:52:32 -0600

    Cambio vista lista de elementos, uso de ListTile

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -31,41 +31,14 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Theme.of(context).primaryColorDark,
-                                width: 2)),
-                        child: Text(
-                          "\$${_transactions[index].amount.toStringAsFixed(2)}", //String interpolation
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColorDark),
-                        ),
-                        padding: EdgeInsets.all(10),
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Text(
-                            _transactions[index].title,
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          Text(
-                            DateFormat().add_yMMMd().format(_transactions[index]
-                                .date), //Codigo exportado de INTL
-                            style: TextStyle(color: Colors.grey[800]),
-                          ),
-                        ],
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                      ),
-                    ],
+                return ListTile(
+                  title: Text(_transactions[index].title),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                        padding: EdgeInsets.all(5),
+                        child: FittedBox(
+                            child: Text("\$${_transactions[index].amount}"))),
                   ),
                 );
               },
